f10c6ea0fdfd92d474f2fb1470f518861b814c62
SantiagoMRodriguez <santiagoorodriguez1@gmail.com>
2023-06-04 22:07:46 -0300

    Clases nuevas Empresa y TipoDeAsiento

---
+++ Clases/Empresa.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Empresa {
+	//Atributos
+	private HashMap<Integer, Micro> listMicro;
+	private String nombre;
+	//Constructor
+	public Empresa(String micro) {
+		this.nombre = nombre;
+		Map<Integer, Micro> listMicro = new HashMap<Integer,Micro>(); 
+	}
+	//Metodos
+	public void setNombre(String nombre){
+		this.nombre = nombre;
+	}
+	public void addMicro(int nro, String nombre){
+		//Codigo
+	}
+	public void eliminarMicro(int nro){
+		//Codigo
+	}
+	public String getNombre() {
+		return nombre;
+	}
+}
---
+++ Clases/TipoDeAsiento.java
@@ -0,0 +1,6 @@
+
+public enum TipoDeAsiento {
+	cama,
+	semicama,
+	ejecutivo
+}
